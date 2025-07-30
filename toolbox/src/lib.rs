24ef765815d75bc1dcab43587e1f2638f3d2b8b0
sineptic <sineptic0@gmail.com>
2025-07-30 21:17:02 +0300

    restart implementation

--- toolbox/src/lib.rs
+++ toolbox/src/lib.rs
@@ -1,23 +1,50 @@
-use std::{env::ArgsOs, iter::Peekable};
+use std::{collections::BTreeMap, ffi::OsString};
 
-mod flag;
-pub use flag::*;
-pub mod flags;
-
-pub fn start_parsing() -> ParsingState<()> {
-    let mut args = std::env::args_os();
-    args.next().unwrap();
-    ParsingState {
-        name: (),
-        args: args.peekable(),
-        flags: (),
-        possible_flags: Default::default(),
+pub struct ParsingContext {
+    pub args: Vec<OsString>,
+    pub cursor: usize,
+    pub partial_arg_pos: Option<usize>,
+}
+impl ParsingContext {
+    pub fn from_args() -> Self {
+        Self {
+            args: std::env::args_os().collect(),
+            cursor: 1,
+            partial_arg_pos: None,
+        }
     }
 }
-#[derive(Debug)]
-pub struct ParsingState<Flags> {
-    pub name: (),
-    pub args: Peekable<ArgsOs>,
-    pub flags: Flags,
-    pub possible_flags: Vec<FlagProperies>,
+
+pub struct Documentation {
+    pub section: &'static str,
+    pub description: &'static str,
+}
+
+pub fn add_documentation(
+    docs: Documentation,
+    store: &mut BTreeMap<&'static str, Vec<&'static str>>,
+) {
+    store
+        .entry(docs.section)
+        .or_default()
+        .push(docs.description);
+}
+pub fn build_documentation(
+    main_description: &'static str,
+    opts: BTreeMap<&'static str, Vec<&'static str>>,
+) -> String {
+    let mut output = String::new();
+    output.push_str(main_description);
+    output.push('\n');
+    for (section, items) in opts {
+        output.push_str(section);
+        output.push_str(":\n");
+        for item in items {
+            output.push_str("    ");
+            output.push_str(item);
+            output.push('\n');
+        }
+    }
+
+    output
 }
--- toolbox/src/main.rs
+++ toolbox/src/main.rs
@@ -1,17 +1,65 @@
-use std::ffi::OsString;
+use anyhow::Result;
+use toolbox::*;
 
-struct _MyArgs {
-    link_name: OsString,
-    to: OsString,
-    destination: OsString,
+fn main() -> Result<()> {
+    parse(main_branch)?;
+    // let var = toolbox::start_parsing().flags3((
+    //     (("hi",), "hello world flag"),
+    //     (("my",), "meeee"),
+    //     (("world", 'w'), "worldldld"),
+    // ));
+    // println!("{var:#?}");
+    // let (_hi, _my, _world) = var.flags;
+    Ok(())
+}
+fn main_branch(FlagHi(is_hi_set): FlagHi) {
+    dbg!(is_hi_set);
 }
 
-fn main() {
-    let var = toolbox::start_parsing().flags3((
-        (("hi",), "hello world flag"),
-        (("my",), "meeee"),
-        (("world", 'w'), "worldldld"),
-    ));
-    println!("{var:#?}");
-    let (_hi, _my, _world) = var.flags;
+fn parse(main_branch: fn(FlagHi)) -> Result<()> {
+    let mut cx = ParsingContext::from_args();
+    let hi_flag = FlagHi::try_parse_self(&mut cx)?;
+    if cx.cursor != cx.args.len() {
+        return Err(anyhow::anyhow!(
+            "unmatched args: '{}'",
+            cx.args[cx.cursor..]
+                .iter()
+                .map(|x| x.to_string_lossy())
+                .collect::<Vec<_>>()
+                .join(" ")
+        ));
+    }
+    let hi_flag = hi_flag
+        .ok_or(String::new())
+        .or_else(|_| FlagHi::default_case())?;
+    main_branch(hi_flag);
+    Ok(())
+}
+
+struct FlagHi(bool);
+impl FlagHi {
+    fn try_parse_self(cx: &mut ParsingContext) -> Result<Option<Self>> {
+        if let Some(flag) = cx.args.get(cx.cursor)
+            && let Some(flag) = flag.to_str()
+            && flag.starts_with("--hi")
+        {
+            cx.cursor += 1;
+            if flag != "--hi" {
+                return Err(anyhow::anyhow!(
+                    "flag '{flag}' not fount. maybe you mean '--hi'"
+                ));
+            }
+            Ok(Some(FlagHi(true)))
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn default_case() -> Result<Self> {
+        Ok(FlagHi(false))
+    }
+    const DOCUMENTATION: Documentation = Documentation {
+        section: "flag",
+        description: "hello world flag",
+    };
 }
