4f270a6d4dc805f154feecb88e30b5ce2ce3b9f9
sineptic <sineptic0@gmail.com>
2025-08-25 18:01:25 +0300

    feat: add https support

--- personal_website/src/main.rs
+++ personal_website/src/main.rs
@@ -1,6 +1,16 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, sync::Arc};
 
 use axum::{Router, routing::get};
+use hyper::{Request, body::Incoming};
+use hyper_util::rt::{TokioExecutor, TokioIo};
+use tokio_rustls::{
+    TlsAcceptor,
+    rustls::{
+        ServerConfig,
+        pki_types::{CertificateDer, PrivateKeyDer, pem::PemObject},
+    },
+};
+use tower_service::Service;
 
 #[tokio::main]
 async fn main() {
@@ -8,9 +18,37 @@ async fn main() {
     let app = Router::new().route("/", get(main_page));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let tcp_listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
-    axum::serve(listener, app).await.unwrap();
+    let tls_acceptor = TlsAcceptor::from(Arc::new(
+        ServerConfig::builder()
+            .with_no_client_auth()
+            .with_single_cert(
+                Vec::from([CertificateDer::from_pem_file("secrets/cert.pem").unwrap()]),
+                PrivateKeyDer::from_pem_file("secrets/key.pem").unwrap(),
+            )
+            .unwrap(),
+    ));
+
+    loop {
+        let tower_service = app.clone();
+        let tls_acceptor = tls_acceptor.clone();
+        let (cnx, _addr) = tcp_listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            let stream = tls_acceptor.accept(cnx).await.unwrap();
+            let stream = TokioIo::new(stream);
+            let hyper_service = hyper::service::service_fn(move |req: Request<Incoming>| {
+                tower_service.clone().call(req)
+            });
+            let ret = hyper_util::server::conn::auto::Builder::new(TokioExecutor::new())
+                .serve_connection_with_upgrades(stream, hyper_service)
+                .await;
+            if let Err(_err) = ret {
+                todo!()
+            }
+        });
+    }
+    // axum::serve(tcp_listener, app).await.unwrap();
 }
 
 static GLOBAL_STYLES: &str = include_str!("style.css");
